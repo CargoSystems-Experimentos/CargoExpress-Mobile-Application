7c5d4511ebc6563c05c31e874271e132953dea05
Luis Angel Anampa Lavado <u202218664@upc.edu.pe>
2026-05-10 18:39:53 -0500

    feat: add implementation flag for production and develop

--- app/src/main/java/com/cargoexpress/app/core/common/Constants.kt
+++ app/src/main/java/com/cargoexpress/app/core/common/Constants.kt
@@ -2,7 +2,19 @@ package com.cargoexpress.app.core.common
 
 
 object Constants {
-    const val BASE_URL = "https://cargoexpress-backend-production.up.railway.app/api/v1/"
+    const val PRODUCTION_BASE_URL = "https://cargoexpress-backend-production.up.railway.app/api/v1/"
+    const val LOCAL_IP = "10.0.2.2"
+    const val LOCAL_PORT = "5194"
+
+    var USE_LOCAL_API = true
+
+    val BASE_URL: String
+        get() = if (USE_LOCAL_API) {
+            "http://$LOCAL_IP:$LOCAL_PORT/api/v1/"
+        } else {
+            PRODUCTION_BASE_URL
+        }
+
     var TOKEN: String = ""
     var USER_ID: Int = 0
     var USER_NAME: String = ""
